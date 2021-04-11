f9f7848ac9211b898fb2d94d650c55c321079f68
RM1322115 <82188019+RM1322115@users.noreply.github.com>
2021-04-10 21:47:05 -0300

    novo

---
+++ EFNgApp/EFNgApp/Migrations/20210411003717_Intial.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFNgApp.Migrations
+{
+    public partial class Intial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tblCities",
+                columns: table => new
+                {
+                    CityID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CityName = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__tblCitie__F2D21A967F3B9C20", x => x.CityID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tblEmployee",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    City = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Department = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    Gender = table.Column<string>(type: "varchar(6)", unicode: false, maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__tblEmplo__7AD04FF1F8EC0A37", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tblCities");
+
+            migrationBuilder.DropTable(
+                name: "tblEmployee");
+        }
+    }
+}
